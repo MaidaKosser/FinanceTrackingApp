39bba890dd68dc7b4398ff29d44a1c8d6aef1b7b
MaidaKosser <maidabutt2004@gmail.com>
2026-05-22 00:49:37 +0500

    Hide the Device Clock Globally and update root navigator

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 // Always keep the global stylesheet import at the absolute top
 import "./global.css";
-
+import { StatusBar } from "react-native";
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
@@ -13,6 +13,7 @@ export default function Index() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <HeroUINativeProvider>
         <ToastProvider>
+          <StatusBar hidden={true} />
           <RootNavigator />
         </ToastProvider>
       </HeroUINativeProvider>
